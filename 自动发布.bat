46d2b54e7534f26b07bd209d90121360cde7bf61
qdx <qdx19qdx@126.com>
2026-05-31 18:33:01 +0800

    Update: 2026/05/31 周日 18:33:01.15

--- 自动发布.bat
+++ 自动发布.bat
@@ -1,27 +1,8 @@
 @echo off
-chcp 65001 >nul
-echo ============== 开始一键同步到GitHub ==============
-
-cd /d D:\myblog || (
-    echo 错误：找不到博客目录
-    pause
-    exit /b 1
-)
-
-if not exist ".git" (
-    echo 错误：不是Git仓库
-    pause
-    exit /b 1
-)
-
-echo 正在添加文件...
+cd /d D:\myblog
+hugo
 git add .
-
-echo 正在提交更改...
-git commit -m "自动发布：%date% %time%"
-
-echo 正在推送到GitHub...
-git push origin master
-
-echo ============== 同步完成 ==============
+git commit -m "Update: %date% %time%"
+git push github master
+echo 推送完成！
 pause
